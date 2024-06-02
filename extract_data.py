a55e25bb21bf530957ec7a62cbc35cd38614104e
Muhammad Usman <61974138+usmandevops123@users.noreply.github.com>
2024-06-02 22:18:42 +0200

    Add files via upload

---
+++ extract_data.py
@@ -0,0 +1,35 @@
+import requests
+import csv
+
+url = "https://cricbuzz-cricket.p.rapidapi.com/stats/v1/rankings/batsmen"
+headers = {
+	"x-rapidapi-key": "20ce69654amsh9f47453073f6b44p1f0a4ejsn5865a79a610b",
+	"x-rapidapi-host": "cricbuzz-cricket.p.rapidapi.com"
+}
+
+params = {
+    'formatType': 'odi'
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+if response.status_code == 200:
+    data = response.json().get('rank', [])  # Extracting the 'rank' data
+    csv_filename = 'batsmen_rankings.csv'
+
+    if data:
+        field_names = ['rank', 'name', 'country']  # Specify required field names
+
+        # Write data to CSV file with only specified field names
+        with open(csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=field_names)
+            #writer.writeheader()
+            for entry in data:
+                writer.writerow({field: entry.get(field) for field in field_names})
+
+        print(f"Data fetched successfully and written to '{csv_filename}'")
+    else:
+        print("No data available from the API.")
+
+else:
+    print("Failed to fetch data:", response.status_code)
